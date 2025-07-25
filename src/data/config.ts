b47cc8ae3e911f714dc454a49affcbe7c3189fa1
nithishkumarr27 <nithish.r.ihub@snsgroups.com>
2025-07-25 11:46:11 +0530

    initial commit

---
+++ src/data/config.ts
@@ -0,0 +1,54 @@
+const config = {
+  title: "Nithish Kumar | Full Stack Developer & DevOps Engineer",
+  description: {
+    long: "Explore the portfolio of Nithish Kumar, a Computer Science Engineer with 1 year experience in Full Stack Development, DevOps, and Database management. Specializing in Three.js, RAG, Agents, Generative AI, and modern web technologies. Strong expertise in JavaScript, Next.js, React.js, WebSocket, MongoDB, MySQL, and more.",
+    short:
+      "Discover the portfolio of Nithish Kumar, a Full Stack Developer and DevOps Engineer passionate about creating innovative solutions.",
+  },
+  keywords: [
+    "Nithish Kumar",
+    "Nithish",
+    "NITHISH",
+    "nithish",
+    "NITHISH KUMAR",
+    "nithish kumar",
+    "Nithish Kumar portfolio",
+    "nithishkumar.com",
+    "nithish kumar portfolio",
+    "portfolio",
+    "Full Stack Developer",
+    "DevOps",
+    "Three.js",
+    "RAG",
+    "Generative AI",
+    "web development",
+    "interactive websites",
+    "React",
+    "Next.js",
+    "websocket",
+    "MongoDB",
+    "MySQL",
+    "software developer",
+    "software engineer",
+    "freelance developer",
+    "three js",
+    "animation",
+    "Framer Motion",
+    "AWS",
+  ],
+  author: "Nithish Kumar",
+  email: "rajnithish27@gmail.com",
+  site: "https://nithishkumar.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/nithishkumar",
+    linkedin: "https://www.linkedin.com/in/nithish-kumar",
+    instagram: "https://www.instagram.com/nithishkumar",
+    facebook: "https://www.facebook.com/nithishkumar",
+    github: "https://github.com/nithishkumar",
+  },
+};
+export { config };
